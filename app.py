23011f3962d57749eadb0adce0559f90331e2a5f
Gogusaivarshith <156275779+Gogusaivarshith@users.noreply.github.com>
2025-07-13 15:04:40 +0530

    added contact form with database

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
-# app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+# Database config
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 
-if __name__ == "__main__":
+# Define a Contact model
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    message = db.Column(db.Text)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    message = request.form['message']
+
+    # Save to database
+    contact = Contact(name=name, message=message)
+    db.session.add(contact)
+    db.session.commit()
+
+    return render_template('success.html', name=name)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
